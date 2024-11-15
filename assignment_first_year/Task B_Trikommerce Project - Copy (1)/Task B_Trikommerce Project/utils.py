38b38dc17dd6cd943170a5485eb388af7deed100
JoshKing85 <joshking1985@hotmail.co.uk>
2024-11-15 17:10:55 +0000

    Task b complete

--- assignment_first_year/Task B_Trikommerce Project - Copy (1)/Task B_Trikommerce Project/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def delete_existing_report():
-    file = "inventory_report_Tshirts.csv"
-    if os.path.exists(file):
-        os.remove(file)
-        print(f"{file} deleted.")
-    else:
-        print(f"{file} does not exist.")
