3446c2bf568c3cf0669bf3e880e866760114ca27
JoshKing85 <joshking1985@hotmail.co.uk>
2024-11-21 18:23:00 +0000

    all code and comments complete. retest for assurance check,  all passed with csv report generated.

--- Task B_Trikommerce Project/sales.py
+++ Task B_Trikommerce Project/sales.py
@@ -1,10 +1,11 @@
 import random
 
 def daily_sales(available_items, inventory_records, current_day):
-
+    # if day is 0 or multiple of 7 no sales made, return available items as updated in previous function call.
     if current_day == 0 or current_day %7 == 0:
         return available_items
     
+    #Generate random value between 0 to 200 as daily sales. Subtract sales from available items, update inventory records.
     else:
         sold_items = random.randint(0, 200)
         available_items -= sold_items
@@ -12,25 +13,4 @@ def daily_sales(available_items, inventory_records, current_day):
         inventory_records = inventory_records.append([current_day, sold_items, 0, available_items])
 
 
-
-
-    '''
-***********COMPLETE THIS FUNCTION***********
-This function is responsible for updating the sales for a given day.
----------------
-Function Input:
----------------
-available_items: (integer) Available Tshirts from the previous day.
-inventory_records: (List) A list of inventory records until the previous day. Each row contains (day, sales, restocked items, available items)
-current_day: (integer) Day number which you want to add as the current day. 
-
-----------------
-Function Output:
-----------------
-available_items:(integer) This function returns this integer which updates the available items at the current day.
-
-The function will also update the inventory_records (For restocking) for a  given current day. 
-
-    '''
-    
     return available_items
